7c49decfa85fcd787b085a3ce5a18565f08f8b45
Max Mucha Morales <maxmuc@outlook.com>
2025-07-03 21:53:29 -0500

    Quinto cambio

--- etl.py
+++ etl.py
@@ -5,4 +5,4 @@ df = pd.read_csv('input.csv')
 df['processed'] = df['value'] * 100
 df['processed_inv'] = df['processed'] / 10
 df.to_csv('output1.csv', index=False)
-print("ETL completado.")
+print("ETL completado en la tercera clase.")
